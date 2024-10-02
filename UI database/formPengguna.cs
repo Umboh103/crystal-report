4f0fdd9471d74a36c70c6b050c1ceffc293cb1fe
Umboh103 <s22310184@student.unklab.ac.id>
2024-10-02 17:23:01 +0800

    Add files via upload

---
+++ UI database/formPengguna.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UI_database
+{
+    public partial class formPengguna : Form
+    {
+        public formPengguna()
+        {
+            InitializeComponent();
+        }
+
+        private void formPengguna_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void crystalReportViewer1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void crystalReportViewer2_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
